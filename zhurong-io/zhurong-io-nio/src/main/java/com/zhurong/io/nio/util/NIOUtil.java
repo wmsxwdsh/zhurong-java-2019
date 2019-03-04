72b6af4b6bf9c0eb032e0532f21b1ab324e55da7
LZG <635555803@qq.com>
2019-03-04 22:02:50 +0800

    添加zhurong-io模块

---
+++ zhurong-io/zhurong-io-nio/src/main/java/com/zhurong/io/nio/util/NIOUtil.java
@@ -0,0 +1,68 @@
+/**   
+ * @Title: NIOUtil.java
+ * @Package: com.zhurong.io.nio.util
+ * @author LZG, liuzhongguochn@gmail.com  
+ * Copyright (c) 2019 https://onezg.cnblogs.com
+ */
+package com.zhurong.io.nio.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @description: NIO文件复制工具(大文件复制的时候效率更显快)
+ * @author LZG
+ * @date 2018/11/6
+ */
+public class NIOUtil {
+
+    /**
+     * @Description: 拷贝
+     * @param src 源文件
+     * @param dst 目标文件
+     * @return void
+     * @author LZG
+     * @date 2018/11/6
+     */
+    public static void copyFileUseNIO(String src, String dst) throws IOException {
+        //声明源文件和目标文件
+        FileInputStream fi = new FileInputStream(new File(src));
+        FileOutputStream fo = new FileOutputStream(new File(dst));
+
+        //获得传输通道channel
+        FileChannel inChannel = fi.getChannel();
+        FileChannel outChannel = fo.getChannel();
+
+        //获得容器buffer
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        while (true) {
+            //判断是否读完文件
+            int eof = inChannel.read(buffer);
+            if (eof == -1) {
+                break;
+            }
+
+            //重设一下buffer的position=0，limit=position
+            buffer.flip();
+
+            //开始写
+            outChannel.write(buffer);
+
+            //写完要重置buffer，重设position=0,limit=capacity
+            buffer.clear();
+        }
+
+        //关闭管道
+        inChannel.close();
+        outChannel.close();
+
+        //关闭流
+        fi.close();
+        fo.close();
+    }
+}
