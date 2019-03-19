a107eb1057efeb43c8b0e81ff404ee93f1e83df0
onezg <6355555803@qq.com>
2019-03-19 16:04:16 +0800

    update

--- zhurong-designpattern/src/main/java/com/zhurong/designpattern/package-info.java
+++ zhurong-designpattern/src/main/java/com/zhurong/designpattern/package-info.java
@@ -8,7 +8,7 @@ package com.zhurong.designpattern;
     抽象工厂
     工厂方法
     建造者模式
-    原型模式
+    原型模式  --
     单态模式  --http://www.cnblogs.com/java-my-life/archive/2012/03/31/2425631.html
 
  二、结构型 - 7
@@ -18,7 +18,7 @@ package com.zhurong.designpattern;
     外观模式
     装饰者模式
     享元模式
-    代理模式
+    代理模式  --静态代理、jdk动态代理/cglib动态代理
 
  三、行为型 - 11
     责任链模式
