c23546ea3af5072823bdbe3384944e71e138c9cf
onezg <6355555803@qq.com>
2019-03-22 17:07:49 +0800

    添加AES加密工具类

---
+++ zhurong-utils/src/main/java/com/zhurong/utils/sign/AESUtil.java
@@ -0,0 +1,117 @@
+/**
+ * @Title: AESUtil.java
+ * @Package: com.zhurong.utils.sign
+ * @author LZG, liuzhongguochn@gmail.com  
+ * Copyright (c) 2019 北京艾森思科技有限公司
+ */
+package com.zhurong.utils.sign;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * @description: AES加密工具类
+ * @author LZG
+ * @date 2019/3/22
+ */
+public class AESUtil {
+
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";//默认的加密算法
+
+    /**
+     * @Description: AES 加密操作
+     * @param content 待加密内容
+     * @param salt 盐
+     * @return java.lang.String 返回Base64转码后的加密数据
+     * @date 2019/3/22
+     */
+    public static String encrypt(String content, String salt) {
+        try {
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);// 创建密码器
+            byte[] byteContent = content.getBytes("utf-8");
+            cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(salt));// 初始化为加密模式的密码器
+
+            byte[] result = cipher.doFinal(byteContent);// 加密
+            return Base64.encodeBase64String(result);//通过Base64转码返回
+        } catch (Exception ex) {
+            Logger.getLogger(AESUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+
+    /**
+     * @Description: AES 解密操作
+     * @param content 加密后的内容
+     * @param salt 盐
+     * @return java.lang.String
+     * @date 2019/3/22
+     */
+    public static String decrypt(String content, String salt) {
+
+        try {
+            //实例化
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+            //使用密钥初始化，设置为解密模式
+            cipher.init(Cipher.DECRYPT_MODE, getSecretKey(salt));
+
+            //执行操作
+            byte[] result = cipher.doFinal(Base64.decodeBase64(content));
+
+            return new String(result, "utf-8");
+        } catch (Exception ex) {
+            Logger.getLogger(AESUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+
+    /**
+     * @Description: 生成加密秘钥
+     * @param password
+     * @return javax.crypto.spec.SecretKeySpec
+     * @author LZG
+     * @date 2019/3/22
+     */
+    private static SecretKeySpec getSecretKey(final String password) {
+        //返回生成指定算法密钥生成器的 KeyGenerator 对象
+        KeyGenerator kg = null;
+
+        try {
+            kg = KeyGenerator.getInstance(KEY_ALGORITHM);
+
+            //AES 要求密钥长度为 128
+            kg.init(128, new SecureRandom(password.getBytes()));
+
+            //生成一个密钥
+            SecretKey secretKey = kg.generateKey();
+
+            return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);// 转换为AES专用密钥
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(AESUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String content = "2019";
+        String salt = "salt1234";
+
+        System.out.println("原文:" + content);
+
+        String s1 = AESUtil.encrypt(content, salt);
+        System.out.println("AES加密后:" + s1);
+
+        System.out.println("AES解密后:" + AESUtil.decrypt(s1, salt));
+    }
+
+}
