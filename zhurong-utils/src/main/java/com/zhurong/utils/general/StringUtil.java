541d8953591e5a0590172ccef4ed29b9a0a7c230
onezg <6355555803@qq.com>
2019-06-04 11:07:11 +0800

    update

--- zhurong-utils/src/main/java/com/zhurong/utils/general/StringUtil.java
+++ zhurong-utils/src/main/java/com/zhurong/utils/general/StringUtil.java
@@ -232,5 +232,40 @@ public class StringUtil {
         return builder.toString();
     }
 
+    //=== Used By AttachFile start ===========
+
+    public static String unescape(String src) {
+        StringBuffer tmp = new StringBuffer();
+        tmp.ensureCapacity(src.length());
+        int lastPos = 0;
+        boolean var3 = false;
+
+        while(lastPos < src.length()) {
+            int pos = src.indexOf("%", lastPos);
+            if (pos == lastPos) {
+                char ch;
+                if (src.charAt(pos + 1) == 'u') {
+                    ch = (char)Integer.parseInt(src.substring(pos + 2, pos + 6), 16);
+                    tmp.append(ch);
+                    lastPos = pos + 6;
+                } else {
+                    ch = (char)Integer.parseInt(src.substring(pos + 1, pos + 3), 16);
+                    tmp.append(ch);
+                    lastPos = pos + 3;
+                }
+            } else if (pos == -1) {
+                tmp.append(src.substring(lastPos));
+                lastPos = src.length();
+            } else {
+                tmp.append(src.substring(lastPos, pos));
+                lastPos = pos;
+            }
+        }
+
+        return tmp.toString();
+    }
+
+    //=== Used By AttachFile end ===========
+
 
 }
