45107e01414f901adcf83726edba4ff9d0f38050
onezg <635555803@qq.com>
2019-07-28 14:11:19 +0800

    纯粹的md5工具类

---
+++ zhurong-utils/src/main/java/com/zhurong/utils/sign/MD5Util.java
@@ -0,0 +1,61 @@
+/**
+ * @Title: MD5Util.java
+ * @Package: com.zhurong.utils.sign
+ * @author LZG, liuzhongguochn@gmail.com  
+ * Copyright (c) 2019 北京艾森思科技有限公司
+ */
+package com.zhurong.utils.sign;
+
+import java.security.MessageDigest;
+
+/**
+ * @description: 纯粹的MD5工具类
+ * @author LZG
+ * @date 2019/7/28
+ */
+public class MD5Util {
+
+    /**
+     * @Description: 字符串进行md5加密
+     * @param inStr
+     * @return java.lang.String
+     * @author LZG
+     * @date 2019/7/28
+     */
+    public static String string2MD5(String inStr) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        char[] charArray = inStr.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++) {
+            byteArray[i] = ((byte) charArray[i]);
+        }
+        byte[] md5Bytes = md5.digest(byteArray);
+
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = md5Bytes[i] & 0xFF;
+            if (val < 16) {
+                hexValue.append("0");
+            }
+            hexValue.append(Integer.toHexString(val));
+        }
+
+        return hexValue.toString();
+    }
+
+    //test
+    public static void main(String[] args) {
+        //md5码：7b9f9e57bc99618dd3368b35a1e7caed
+        String pwd = "hdswj68$fxxt28";
+        System.out.println(string2MD5(pwd));
+    }
+
+}
