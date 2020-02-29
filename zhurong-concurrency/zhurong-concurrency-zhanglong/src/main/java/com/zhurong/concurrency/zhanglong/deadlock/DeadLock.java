b0811ccbc0b30fdbc77d7d6a6ba2c8226d6314da
liuzhongguo <liuzhongguo@cdvcloud.com>
2020-02-29 10:19:29 +0800

    update

---
+++ zhurong-concurrency/zhurong-concurrency-zhanglong/src/main/java/com/zhurong/concurrency/zhanglong/deadlock/DeadLock.java
@@ -0,0 +1,74 @@
+/**
+ * @Title: DeadLock.java
+ * @Package: com.zhurong.concurrency.zhanglong.deadlock
+ * @author LZG, liuzhongguo@cdvcloud.com
+ * Copyright (c) 2020 https://www.yunshicloud.com
+ */
+package com.zhurong.concurrency.zhanglong.deadlock;
+
+/**
+ * @description: 死锁
+ * @author LZG
+ * @date 2020年02月28日
+ */
+public class DeadLock {
+
+    private Object lock1 = new Object();
+    private Object lock2 = new Object();
+
+    public void myMethod1() {
+        synchronized (lock1) {
+            synchronized (lock2) {
+                System.out.println("myMethod1 invoked");
+            }
+        }
+    }
+
+    public void myMethod2() {
+        synchronized (lock2) {
+            synchronized (lock1) {
+                System.out.println("myMethod2 invoked");
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        DeadLock deadLock = new DeadLock();
+
+        Runnable runnable1 = () -> {
+            while(true) {
+                deadLock.myMethod1();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+
+        Thread thread1 = new Thread(runnable1, "myThread1");
+
+
+        Runnable runnable2 = () -> {
+            while(true) {
+                deadLock.myMethod2();
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+
+        Thread thread2 = new Thread(runnable2, "myThread2");
+
+        thread1.start();
+        thread2.start();
+
+    }
+
+}
+
