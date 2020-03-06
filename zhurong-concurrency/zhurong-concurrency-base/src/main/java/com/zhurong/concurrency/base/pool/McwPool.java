c5b7559d65f8435c21cd055de5402edc237c59de
liuzhongguo <liuzhongguo@cdvcloud.com>
2020-03-06 11:44:57 +0800

    format

--- zhurong-concurrency/zhurong-concurrency-base/src/main/java/com/zhurong/concurrency/base/pool/McwPool.java
+++ zhurong-concurrency/zhurong-concurrency-base/src/main/java/com/zhurong/concurrency/base/pool/McwPool.java
@@ -13,8 +13,8 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @description: 马才伟试验例子
  * @author LZG
+ * @description: 马才伟试验例子
  * @date 2020年03月06日
  */
 public class McwPool {
@@ -22,9 +22,9 @@ public class McwPool {
     @Test
     public void test1() throws InterruptedException {
 
-        ThreadPoolExecutor threadPoolExecutor=new ThreadPoolExecutor(
-                3,3,2,
-                TimeUnit.MINUTES,new ArrayBlockingQueue<Runnable>(10));
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                3, 3, 2,
+                TimeUnit.MINUTES, new ArrayBlockingQueue<Runnable>(10));
 
 //        threadPoolExecutor.prestartAllCoreThreads();
 
@@ -42,9 +42,8 @@ public class McwPool {
 
 /**
  * 新方东面试题：
- *
+ * <p>
  * 线程池 假如我现在核心线程数5 最大线程数10 阻塞队列50
  * 如果来了一个task 启了一个线程并且执行完了 然后又来了一个任务
  * 它会使用之前那个线程还是重新创建一个线程？
- *
  */
