f51381fc523266d5df6e74e645d5be9e36558b86
liuzhongguo <liuzhongguo@cdvcloud.com>
2020-06-02 21:20:05 +0800

    mq

---
+++ zhurong-mq/src/test/java/com/MyTest.java
@@ -0,0 +1,18 @@
+/**
+ * @Title: MyTest.java
+ * @Package: com
+ * @author LZG, liuzhongguo@cdvcloud.com
+ * Copyright (c) 2020 https://www.yunshicloud.com
+ */
+package com;
+
+/**
+ * @description:
+ * @author LZG
+ * @date 2020年06月02日
+ */
+public class MyTest {
+
+
+    public static volatile MyTest myTest = null;
+}
