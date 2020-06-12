2ff1f8978664c2a15ed5e4b708b77d81dbb5c529
onezg <635555803@qq.com>
2020-06-12 20:25:24 +0800

    神州顺利办start

---
+++ zhurong-slb/src/main/java/com/zhurong/slb/MainApplication.java
@@ -0,0 +1,25 @@
+/**
+ * @Title: MainApplication.java
+ * @Package: com.zhurong.slb
+ * @author LZG, liuzhongguo@cdvcloud.com
+ * Copyright (c) 2020 https://www.yunshicloud.com
+ */
+package com.zhurong.slb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author LZG
+ * @date 2020年06月11日
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
