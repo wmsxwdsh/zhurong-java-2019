0efe5b561371c46d8b177ec744631510087af605
liuzg@12366.com <Java12366>
2021-08-02 10:14:08 +0800

    提交两个常用的工具类

---
+++ zhurong-mybatis/src/main/java/com/zhurong/mybatis/utils/PageUtils.java
@@ -0,0 +1,82 @@
+package com.zhurong.mybatis.utils;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author LZG
+ * @date 2021/8/2
+ */
+public class PageUtils<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int totalCount;
+    private int pageSize;
+    private int totalPage;
+    private int currPage;
+    private List<T> list;
+
+    public PageUtils() {
+    }
+
+    public PageUtils(int currPage, int pageSize) {
+        this.currPage = currPage;
+        this.pageSize = pageSize;
+    }
+
+    public PageUtils(List<T> list, int totalCount, int pageSize, int currPage) {
+        this.list = list;
+        this.totalCount = totalCount;
+        this.pageSize = pageSize;
+        this.currPage = currPage;
+        this.totalPage = (int)Math.ceil((double)totalCount / (double)pageSize);
+    }
+
+    public PageUtils(IPage<T> page) {
+        this.list = page.getRecords();
+        this.totalCount = (int)page.getTotal();
+        this.pageSize = (int)page.getSize();
+        this.currPage = (int)page.getCurrent();
+        this.totalPage = (int)page.getPages();
+    }
+
+    public int getTotalCount() {
+        return this.totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public int getPageSize() {
+        return this.pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getTotalPage() {
+        return this.totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public int getCurrPage() {
+        return this.currPage;
+    }
+
+    public void setCurrPage(int currPage) {
+        this.currPage = currPage;
+    }
+
+    public List<T> getList() {
+        return this.list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
---
+++ zhurong-mybatis/src/main/java/com/zhurong/mybatis/utils/PojoUtils.java
@@ -0,0 +1,63 @@
+package com.zhurong.mybatis.utils;
+
+import org.springframework.beans.BeanUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author LZG
+ * @date 2021/8/2
+ */
+public class PojoUtils<V, E> {
+    public PojoUtils() {
+    }
+
+    public static <E> E convertPojo(Object srcClass, Class<E> tarClass) {
+        if (srcClass == null) {
+            return null;
+        } else if (tarClass == null) {
+            return null;
+        } else {
+            try {
+                E newInstance = tarClass.newInstance();
+                BeanUtils.copyProperties(srcClass, newInstance);
+                return newInstance;
+            } catch (Exception var3) {
+                return null;
+            }
+        }
+    }
+
+    public static <T, V> PageUtils<V> pageVo(PageUtils<T> srcPage, Class<V> tarClass) {
+        List<V> voList = (List)srcPage.getList().stream().map((item) -> {
+            try {
+                return convertPojo(item, tarClass.newInstance().getClass());
+            } catch (InstantiationException var3) {
+                var3.printStackTrace();
+            } catch (IllegalAccessException var4) {
+                var4.printStackTrace();
+            }
+
+            return null;
+        }).collect(Collectors.toList());
+        PageUtils<V> tarPage = new PageUtils(voList, srcPage.getTotalCount(), srcPage.getPageSize(), srcPage.getCurrPage());
+        tarPage.setList(voList);
+        return tarPage;
+    }
+
+    public static <T, V> List<V> listPojo(List<T> srcList, Class<V> tarClass) {
+        List<V> voList = (List)srcList.stream().map((item) -> {
+            try {
+                return convertPojo(item, tarClass.newInstance().getClass());
+            } catch (InstantiationException var3) {
+                var3.printStackTrace();
+            } catch (IllegalAccessException var4) {
+                var4.printStackTrace();
+            }
+
+            return null;
+        }).collect(Collectors.toList());
+        return voList;
+    }
+}
