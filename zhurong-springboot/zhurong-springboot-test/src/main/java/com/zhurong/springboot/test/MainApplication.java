d79c30ebb9bb834f7e0bcf571728d8bfa718393d
刘仲国 <635555803@qq.com>
2022-04-01 18:49:34 +0800

    update

--- zhurong-springboot/zhurong-springboot-test/src/main/java/com/zhurong/springboot/test/MainApplication.java
+++ zhurong-springboot/zhurong-springboot-test/src/main/java/com/zhurong/springboot/test/MainApplication.java
@@ -8,7 +8,6 @@ package com.zhurong.springboot.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @description:
@@ -16,7 +15,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @date 2020年03月09日
  */
 @SpringBootApplication
-//@EnableSwagger2
 public class MainApplication {
 
     public static void main(String[] args) {
--- zhurong-springboot/zhurong-springboot-test/src/main/java/com/zhurong/springboot/test/cookie/TestCookieController.java
+++ zhurong-springboot/zhurong-springboot-test/src/main/java/com/zhurong/springboot/test/cookie/TestCookieController.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -16,6 +17,34 @@ import java.io.IOException;
  */
 @RestController
 public class TestCookieController {
+
+    @GetMapping(value = "/doGet2")
+    public void doGet2(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        //使用request对象的getSession()获取session，如果session不存在则创建一个
+        HttpSession session = request.getSession();
+        //将数据存储到session中
+        session.setAttribute("mayun", "马云");
+        //获取session的Id
+        String sessionId = session.getId();
+        //判断session是不是新创建的
+        if (session.isNew()) {
+            response.getWriter().print("session创建成功，session的id是："+sessionId);
+        }else {
+            response.getWriter().print("服务器已经存在该session了，session的id是："+sessionId);
+        }
+    }
+
+    /**
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     * 2022.04.01
+     */
     @GetMapping(value = "/doGet")
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Cookie cookie = new Cookie("admin", System.currentTimeMillis() + "");
