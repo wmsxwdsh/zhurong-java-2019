a766305a4cc32d34a4196bfc0b21954aa8cd037c
wmsxwdsh <125259411+wmsxwdsh@users.noreply.github.com>
2026-02-06 13:51:16 +0800

    update

--- zhurong-common-utils/src/main/java/com/zhurong/utils/common/DeviceUtil.java
+++ zhurong-common-utils/src/main/java/com/zhurong/utils/common/DeviceUtil.java
@@ -4,19 +4,18 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * 移动设备工具类
- * @author LZG
- * @date 2018/11/12
+ * author LZG
+ * date  2018/11/12
  */
 public class DeviceUtil {
 
     /**
      * 判断是否是移动设备
-     * @date 2018/11/12
      */
     public static boolean isMobileDevice(HttpServletRequest request) {
         String userAgent = request.getHeader("user-agent").toLowerCase();
 
-        //anroid手机
+        //android手机
         if (userAgent.contains("android"))
             return true;
         //苹果手机
@@ -28,6 +27,7 @@ public class DeviceUtil {
         //联想手机
         if (userAgent.contains("lenovo"))
             return true;
+
         return false;
     }
 
--- zhurong-common-utils/src/main/java/com/zhurong/utils/gen/EmailGen.java
+++ zhurong-common-utils/src/main/java/com/zhurong/utils/gen/EmailGen.java
@@ -1,32 +1,24 @@
-/**   
- * @Title: EmailGen.java
- * @Package: com.zhurong.utils.gen
- * @author LZG, liuzhongguochn@gmail.com  
- * Copyright (c) 2019 北京艾森思科技有限公司
- */
 package com.zhurong.utils.gen;
 
 /**
- * @description: 电子邮箱生成器
- * @author LZG
- * @date 2018/11/5
+ * 电子邮箱生成器
+ * 2018/11/5
  */
 public class EmailGen {
 
-    private static String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+    private static final String base = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-    private static final String[] email_suffix="@gmail.com,@yahoo.com,@msn.com,@hotmail.com,@aol.com,@ask.com,@live.com,@qq.com,@0355.net,@163.com,@163.net,@263.net,@3721.net,@yeah.net,@googlemail.com,@126.com,@sina.com,@sohu.com,@yahoo.com.cn".split(",");
+    private static final String[] email_suffix = ("@gmail.com, @yahoo.com, @msn.com, @hotmail.com, @aol.com, @ask.com, " +
+            "@live.com, @qq.com, @0355.net, @163.com, @163.net, @263.net, @3721.net, @yeah.net, @googlemail.com, @126.com, " +
+            "@sina.com, @sohu.com, @yahoo.com.cn").split(",");
 
     /**
-     * @Description: 生成email
-     * @return java.lang.String
-     * @author LZG
-     * @date 2018/11/5
+     * 生成 email
      */
     public static String generate() {
         //生成4到12之间任意一个数字个的字符
         int length = getNum(4, 12);
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) {
             int number = (int) (Math.random() * base.length());
             sb.append(base.charAt(number));
@@ -37,10 +29,7 @@ public class EmailGen {
     }
 
     /**
-     * @Description: 生成任一数字(最小start，最大end）
-     * @return int
-     * @author LZG
-     * @date 2018/11/5
+     * 生成 start-end 的任一数字
      */
     private static int getNum(int start, int end) {
         return (int) (Math.random() * (end - start + 1) + start);
